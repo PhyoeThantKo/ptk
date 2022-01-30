0203bd25ef7aa0d479af41dc9bdebaf0982b420d
Phyoe Thant Ko <84618795+PhyoeThantKo@users.noreply.github.com>
2022-01-30 16:16:40 +0630

    add_navbar

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" class="font-mono">
+<html lang="en" class="font-roboto">
 <head>
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,7 +9,43 @@
      <link rel="stylesheet" href="app/carousel/style.css">
 </head>
 <body>
+
+    <!-- nav bar -->
+    <div id="nav_container" class="absolute w-full mt-3 z-50">
+      <nav class="flex w-11/12 h-11 rounded-l-full rounded-r-full bg-earth mx-auto">
+        <div id="logo" class="w-1/2 my-auto ml-6">
+          <a href="index.html" class="text-2xl font-extrabold hover:text-light_theme">
+            PTK
+            <span class="font-light text-sm">
+              PRODUCT DESIGN + WEB DEV
+            </span>
+          </a>
+        </div>
+        <div id="nav_items" class="w-1/2 my-auto md:text-2xl">
+          <ul class="flex lg:ml-48">
+            <li class="w-1/5 hover:text-light_theme text-center text-xl">
+              <a href="">
+                Home
+              </a>
+            </li>
+            <li class="w-1/5 hover:text-light_theme text-center text-xl">
+              <a href="" >Portfolio</a>
+            </li>
+            <li class="w-1/5 hover:text-light_theme text-center text-xl">
+              <a href="" >Contact</a>
+            </li>
+            <li class="w-1/5 hover:text-light_theme text-center text-xl">
+              <a href="" >About</a>
+            </li>
+            <li class="w-1/5 hover:text-yellow-500 text-center text-xl text-light_theme">
+              <a href="">Articles</a>
+            </li>
+          </ul>
+        </div>
   
+      </nav>
+
+  </div>
   <!-- carousel -->
   <section class="has-dflex-center">
     <div class="lx-container-80">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,11 @@ module.exports = {
     extend: {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
-        encodesans: ["Encode Sans", "sans-serif"]
+        roboto: ["Roboto", "sans-serif"]
       },
       colors: {
         'earth': '#F9E8DA',
+        'light_theme': '#DB1084',
       }
     },
   },
