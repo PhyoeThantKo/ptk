91157bb02c3c8b6df3e0aacb3792f8bb64d36857
Phyoe Thant Ko <84618795+PhyoeThantKo@users.noreply.github.com>
2022-02-04 14:53:52 +0630

    complete_carousel

--- index.php
+++ index.php
@@ -21,47 +21,94 @@
             </span>
           </a>
         </div>
-        <div id="nav_items" class="w-1/2 my-auto md:text-2xl">
+        <div id="nav_items" class="w-1/2 my-auto">
           <ul class="flex lg:ml-48">
-            <li class="w-1/5 hover:text-light_theme text-center text-xl">
+            <li class="w-1/5 hover:text-light_theme text-center text-base lg:text-xl text-gray-700">
               <a href="">
                 Home
               </a>
             </li>
-            <li class="w-1/5 hover:text-light_theme text-center text-xl">
+            <li class="w-1/5 hover:text-light_theme text-center text-base lg:text-xl text-gray-700">
               <a href="" >Portfolio</a>
             </li>
-            <li class="w-1/5 hover:text-light_theme text-center text-xl">
+            <li class="w-1/5 hover:text-light_theme text-center text-base lg:text-xl text-gray-700">
               <a href="" >Contact</a>
             </li>
-            <li class="w-1/5 hover:text-light_theme text-center text-xl">
+            <li class="w-1/5 hover:text-light_theme text-center text-base lg:text-xl text-gray-700">
               <a href="" >About</a>
             </li>
-            <li class="w-1/5 hover:text-yellow-500 text-center text-xl text-light_theme">
+            <li class="w-1/5 hover:text-yellow-500 text-center text-base lg:text-xl text-light_theme">
               <a href="">Articles</a>
             </li>
           </ul>
         </div>
-  
       </nav>
+    </div>
 
-  </div>
   <!-- carousel -->
   <section class="has-dflex-center">
     <div class="lx-container-80">
       <div class="lx-row">
         <div class="lx-card carousel-container">
           <div class="item fade">
-            <div class="image image1"></div>
+            <div class="image image1">
+              <div id="articles" class="h-52 mt-72 w-4/6 text-7xl ml-16">
+                <p class="text-white font-serif font-medium">
+                  Web Dev articles & </br> <span class="tracking-wide">consumer electronics contents</span>
+                </p>
+                <a href="" class="">
+                  <button type="button" class="bg-light_theme hover:bg-pink-700 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-40 h-12 transition ease-in duration-200 text-center text-xl font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-full">
+                    Read
+                  </button>
+                </a>
+              </div>
+            </div>
           </div>
           <div class="item fade">
-            <div class="image image2"></div>
+            <div class="image image2">
+              <div id="services" class="h-60 w-4/6 text-7xl mx-auto my-auto">
+                <p class="text-white text-center font-serif font-medium tracking-wide">
+                  Digital Solutions for </br> <span class="tracking-wide">Business Transformations</span>
+                </p>
+                <div class="w-full grid place-items-center">
+                  <a href="" class="">
+                    <button type="button" class="bg-light_theme mx-auto hover:bg-pink-700 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-40 h-12 transition ease-in duration-200 text-center text-xl font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full">
+                      Services
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="item fade">
-            <div class="image image3"></div>
+            <div class="image image3">
+              <div id="portfolio" class="h-52 absolute bottom-20 w-4/6 text-7xl ml-16">
+                <p class="text-white font-serif font-medium">
+                  Portfolio </br>A look at some of my projects
+                </p>
+                <a href="" class="">
+                  <button type="button" class="bg-light_theme hover:bg-pink-700 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-40 h-12 transition ease-in duration-200 text-center text-xl font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-full">
+                    Portfolio
+                  </button>
+                </a>
+              </div>
+            </div>
           </div>
           <div class="item fade">
-            <div class="image image4"></div>
+            <div class="image image4">
+              <div id="contact" class="h-60 w-4/6 text-7xl mx-auto grid place-self-end pb-72">
+                <p class="text-white text-center font-serif font-medium tracking-wide">
+                  Let's Talk </br> Anyting big or small
+                </p>
+                <div class="w-full grid place-items-center">
+                  <a href="" class=""> 
+                    <button type="button" class="bg-light_theme mx-auto hover:bg-pink-700 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-40 h-12 transition ease-in duration-200 text-center text-xl font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full">
+                      Contact
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
           <a class="prev has-dflex-center"><i class="fas fa-angle-left"></i></a>
           <a class="next has-dflex-center"><i class="fas fa-angle-right"></i></a>
@@ -70,7 +117,9 @@
     </div>
   </section>
 
-<script src='https://use.fontawesome.com/releases/v5.14.0/js/all.js'></script><script  src="app/carousel/script.js"></script>
+<script src='https://use.fontawesome.com/releases/v5.14.0/js/all.js'></script>
+<script  src="app/carousel/script.js"></script>
+<script src="index.js"></script>
 
 </body>
 </html>
