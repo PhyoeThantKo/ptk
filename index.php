f56dc895571f59562646171c5362dea1863dd8e5
Phyoe Thant Ko <84618795+PhyoeThantKo@users.noreply.github.com>
2022-02-22 10:49:58 +0630

    add_social_links

--- index.php
+++ index.php
@@ -355,19 +355,19 @@
                         Social Media
                     </h2>
                     <div class="grid grid-cols-4 gap-y-4 place-items-center max-w-xs mx-auto">
-                      <a href="#">
+                      <a href="https://t.me/ptk7858">
                         <svg class="h-7 w-7 hover:text-gray-600 text-gray-400 dark:hover:text-white transition-colors duration-200"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4" /></svg>
                       </a>
-                      <a href="#">
+                      <a href="https://www.facebook.com/ptk7858">
                         <svg class="h-7 w-7 hover:text-gray-600 text-gray-400 dark:hover:text-white transition-colors duration-200"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" /></svg>
                       </a>
-                      <a href="#">
+                      <a href="https://github.com/PhyoeThantKo">
                         <svg class="h-7 w-7 hover:text-gray-600 text-gray-400 dark:hover:text-white transition-colors duration-200"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>
                       </a>
-                      <a href="#">
+                      <a href="https://www.instagram.com/ptk__ig/">
                         <svg class="h-7 w-7 hover:text-gray-600 text-gray-400 dark:hover:text-white transition-colors duration-200"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />  <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />  <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" /></svg>
                       </a>
-                      <a href="#">
+                      <a href="https://twitter.com/PTK7858">
                         <svg class="h-7 w-7 hover:text-gray-600 text-gray-400 dark:hover:text-white transition-colors duration-200"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" /></svg>
                       </a>
                   </div>
