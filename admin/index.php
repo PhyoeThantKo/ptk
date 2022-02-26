ce609813a767e8edc45c13f690a2c6b4485fed3a
Phyoe Thant Ko <84618795+PhyoeThantKo@users.noreply.github.com>
2022-02-26 15:59:54 +0630

    start_projectandservice_system

--- admin/index.php
+++ admin/index.php
@@ -26,6 +26,12 @@
      
                               <span class="mx-4 font-medium">New Post</span>
                          </a>
+
+                         <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-200 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700" href="post-new.php">
+                              <img src="images/project.svg">
+     
+                              <span class="mx-4 font-medium">Projects and Services</span>
+                         </a>
      
                          <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-200 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700" href="tags.php">
                               <img src="images/category.svg">
---
+++ admin/newproject.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>New Project or Service</title>
+     <link rel="stylesheet" href="../public/style/style.css">
+</head>
+<body class="bg-notsowhite">
+
+    <section class="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+        <h2 class="text-lg font-semibold text-gray-700 capitalize dark:text-white">Add new Project or Service</h2>
+        
+        <form action="addproject.php" method="POST" enctype="multipart/form-data">
+            <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                <div>
+                    <label class="text-gray-700 dark:text-gray-200" for="header">Header</label>
+                    <input name="header" id="header" type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
+                </div>
+
+                <div>
+                    <label class="text-gray-700 dark:text-gray-200" for="description">Description</label>
+                    <input name="description" id="description" type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
+                </div>
+
+                <div>
+                    <label for="cover" class="text-gray-700 mr-3">
+                         Choose cover
+                    </label>
+
+                    <input name="cover" id="cover" type="file" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
+                </div>
+
+                <div>
+                    <label for="tag">Choose Tag</label>
+                    <select id="tag" name="tag" class="py-1 px-4 h-full pl-2 pr-7 bg-transparent text-gray-500 sm:text-sm">
+                    <option>
+                        project
+                    </option>
+                    <option>
+                        service
+                    </option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="flex justify-end mt-6">
+                <button class="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
+            </div>
+        </form>
+    </section>
+</body>
+</html>
